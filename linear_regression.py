21539af030f9b1d2289195f0fdf99ad9c90d17bd
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-09 18:50:39 -0600

    Assiignment complete

--- linear_regression.py
+++ linear_regression.py
@@ -1,12 +1,14 @@
 #goal: how does a fico score impact interest rates
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
 
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 
 cleanInterestRate = loansData['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 4))
 loansData['Interest.Rate'] = cleanInterestRate
-#[]
 
 cleanLoanLength = loansData['Loan.Length'].map(lambda x: int(x.rstrip(' months')))
 loansData['Loan.Length'] = cleanLoanLength
@@ -15,4 +17,35 @@ cleanFICORange = loansData['FICO.Range'].map(lambda x: x.split('-'))
 cleanFICORange = cleanFICORange.map(lambda x: [int(n) for n in x])
 loansData['FICO.Score'] = [x[0] for x in cleanFICORange]
 
-#print loansData['FICO.Score'][0:5]
+'''
+#histogram
+plt.figure()
+p = loansData['FICO.Score'].hist()
+plt.show()
+'''
+
+'''
+scatterplot
+a = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
+plt.show()
+data says we should use fico score and loan amount as independent variables. Dependent variable, interest rate
+'''
+
+#Grab data that we need from cleaned data frame. Extracting data from data frame creates series, don't forget to transpose
+intrate = loansData['Interest.Rate']
+loanamt = loansData['Amount.Requested']
+fico = loansData['FICO.Score']
+
+#Dependent variable
+y = np.matrix(intrate).transpose()
+#Independent variables
+x1 = np.matrix(fico).transpose()
+x2 = np.matrix(loanamt).transpose()
+
+x = np.column_stack([x1, x2])
+
+X = sm.add_constant(x)
+model = sm.OLS(y, X)
+f = model.fit()
+
+f.summary()
